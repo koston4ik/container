002987c9a7d9977765713490d1c9f4cdb0356d60
koston4ik <allkontiy@gmail.com>
2025-11-01 14:56:16 +0300

    Реализация контейнера с добавлением и извлечением элементов + тесты

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -1,4 +1,52 @@
 
-public class Container {
+public class Container<T> {
+	private static class Node<T> {
+		private T data;
+        private Node<T> next;
 
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+	
+	private Node<T> head;
+    private int size;
+    
+    public Container() {
+        this.head = null;
+        this.size = 0;
+    }
+    
+    public T GetItem(int index) {
+    	if(index < 0 || index >= size) {
+    		throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    	}
+    	
+    	Node<T> current = head;
+    	for(int i = 0; i < index; ++i) {
+    		current = current.next;
+    	}
+    	return current.data;
+    }
+    
+    public int GetSize() {
+    	return size;
+    }
+    
+    public void Add(T item) {
+    	Node<T> newNode = new Node<>(item);
+    	
+    	if(head == null) {
+    		head = newNode;
+    	}
+    	else {
+    		Node<T> current = head;
+    		while (current.next != null) {
+    			current = current.next;
+    		}
+    		current.next = newNode;
+    	}
+    	size++;
+    }
 }
--- src/test/java/ContainerTest.java
+++ src/test/java/ContainerTest.java
@@ -1,7 +1,20 @@
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class ContainerTest {
 	
+	private Container<String> container;
+	
+	@Before
+    public void setUp() {
+        container = new Container<>();
+    }
+	
+	@Test
+    public void testAddAndGet() {
+        container.Add("Hello");
+        assertEquals("Hello", container.GetItem(0));
+        assertEquals(1, container.GetSize());
+    }
 }
